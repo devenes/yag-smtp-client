e40e20a2027c196dd31199dd88dd22a42247054a
Enes <66560757+devenes@users.noreply.github.com>
2022-05-06 00:58:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
 from config import config
 import logging
 import yagmail
+import glob
+import os
+
 
 logging.basicConfig(
     filename=config['log_file'],
     level=config['log_level'])
+
+# Directory where list will be saved
+os.chdir(r"C:\Users\AE\Desktop")
+
+# Define the extension of the files to be sent
+extension = "*.exe"
+
+# Find all files with the extension in this directory
+liste = glob.glob("C:\Windows\System32\%(extension)s" % locals())
+
+f = open('list.txt', 'w')
+for x in liste:
+    # write files' path to the file
+    f.write(x + '\n')
+    # write files' name to the file
+    string_bol = x.split("\\", 3)
+    # Write to console
+    print(string_bol[3])
+    # Get the files' name
+    dosya_isim = string_bol[3]
+    f.write(dosya_isim)
+    f.write('\n')
 
 username = config['user']
 password = config['password']
@@ -17,15 +42,18 @@ print("User registred successfully")
 reciver = input("Please write the reciver email: ")
 subs = input("Please write the subject: ")
 cons = input("Please write the contents: ")
+attach = 'list.txt'
 
 reciverData = reciver
 subjectData = ("'"+subs+"'")
 contentsData = ("'"+cons+"'")
+attachData = attach
 
 try:
     yag.send(to=reciverData,
              subject=subjectData,
-             contents=contentsData)
+             contents=contentsData,
+             attachments=attachData)
     print("Email sent successfully")
 
 # Example: Error:  Emailaddress "adsassmail.com" is not valid according to RFC 2822 standards
